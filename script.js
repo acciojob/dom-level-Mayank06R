54a5944dd9a0432e9d1f8718bb7c0fe717512876
Mayank06R <85189108+Mayank06R@users.noreply.github.com>
2025-02-28 20:27:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    var element = document.getElementById("level");
+    var level = 0;
+    while (element) {
+        level++;
+        element = element.parentElement;
+    }
+    alert("The level of the element is: " + level);
+});
